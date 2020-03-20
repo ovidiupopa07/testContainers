bbb00740bbc2d79f8ffa3fcd82600df765efaa1a
Ovidiu-Mihai Popa <ovidiu.popa@endava.com>
2020-03-20 09:53:35 +0200

    First commit. Create spring boot project
    - reactive web
    - reactive cassandra
    - actuator

---
+++ test-containers-demo/src/main/java/com/example/testcontainers/TestContainersDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestContainersDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestContainersDemoApplication.class, args);
+	}
+
+}
---
+++ test-containers-demo/src/test/java/com/example/testcontainers/TestContainersDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestContainersDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
