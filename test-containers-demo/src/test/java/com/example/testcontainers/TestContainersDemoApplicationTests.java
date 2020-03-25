276efa6d9da2ac7f4613b88dab99253eacb34997
Ovidiu-Mihai Popa <ovidiu.popa@endava.com>
2020-03-25 17:01:49 +0200

    formating

--- test-containers-demo/src/test/java/com/example/testcontainers/TestContainersDemoApplicationTests.java
+++ test-containers-demo/src/test/java/com/example/testcontainers/TestContainersDemoApplicationTests.java
@@ -3,17 +3,13 @@ package com.example.testcontainers;
 import java.util.function.Consumer;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.testcontainers.containers.CassandraContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -26,32 +22,28 @@ import com.example.testcontainers.repository.TestClassRepository;
 @ContextConfiguration(initializers = TestContainersDemoApplicationTests.Initializer.class)
 @Profile("testcontainer")
 class TestContainersDemoApplicationTests {
-	
+
 	@Container
 	private static final CassandraContainer cassandraContainer = new CassandraContainer<>()
-	.withInitScript("insert.cql");
-	
+			.withInitScript("insert.cql");
+
 	@Autowired
 	TestClassRepository testClassRepository;
-	
 
-	
-	static class Initializer
-    implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-      public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-          TestPropertyValues.of(
-            "spring.data.cassandra.contact-points=" + cassandraContainer.getContainerIpAddress(),
-            "spring.data.cassandra.port=" + cassandraContainer.getMappedPort(9042),
-            "spring.data.cassandra.keyspace-name=" + "test",
-            "spring.data.cassandra.cluster-name=" + cassandraContainer.getCluster().getClusterName(),
-            "spring.data.cassandra.username=" + cassandraContainer.getUsername(),
-            "spring.data.cassandra.password=" + cassandraContainer.getPassword()
-          ).applyTo(configurableApplicationContext.getEnvironment());
-      }
-  }
-	
-	
-	
+	static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+		public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+			TestPropertyValues
+					.of("spring.data.cassandra.contact-points=" + cassandraContainer.getContainerIpAddress(),
+							"spring.data.cassandra.port=" + cassandraContainer.getMappedPort(9042),
+							"spring.data.cassandra.keyspace-name=" + "test",
+							"spring.data.cassandra.cluster-name=" + cassandraContainer.getCluster().getClusterName(),
+							"spring.data.cassandra.username=" + cassandraContainer.getUsername(),
+							"spring.data.cassandra.password=" + cassandraContainer.getPassword())
+					.applyTo(configurableApplicationContext.getEnvironment());
+		}
+	}
+
 	@Test
 	void contextLoads() {
 		testClassRepository.findAll().doOnNext(new Consumer<TestClass>() {
