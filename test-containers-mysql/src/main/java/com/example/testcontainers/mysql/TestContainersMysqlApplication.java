dc89faf697945b78bdc999bffa6b46e8208967eb
Ovidiu-Mihai Popa <ovidiu.popa@endava.com>
2020-03-26 15:17:20 +0200

    Clean up pom.xml files Add new module test-containers-mysql

---
+++ test-containers-mysql/src/main/java/com/example/testcontainers/mysql/TestContainersMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcontainers.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestContainersMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestContainersMysqlApplication.class, args);
+    }
+
+}
---
+++ test-containers-mysql/src/test/java/com/example/testcontainers/mysql/TestContainersMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testcontainers.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestContainersMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
